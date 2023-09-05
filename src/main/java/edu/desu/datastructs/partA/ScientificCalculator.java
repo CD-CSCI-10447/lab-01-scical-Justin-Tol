a4b7f74db19802617938fcda14ede64bf69ed084
justint <justintolbertpro@gmail.com>
2023-09-05 05:07:10 -0400

    Problem.java and ScientificCalculator.java has solutions coded, tests are all working.

--- src/main/java/edu/desu/datastructs/partA/ScientificCalculator.java
+++ src/main/java/edu/desu/datastructs/partA/ScientificCalculator.java
@@ -1,62 +1,58 @@
 package edu.desu.datastructs.partA;
-
+import java.lang.Math;
 public class ScientificCalculator {
 
     // This method should add two numbers: x and y, and return the result.
     public double add(double x, double y){
-        // TODO: Implement the addition operation.
-        return -1;
+        return x + y;
     }
 
     // This method should subtract y from x and return the result.
     public double subtract(double x, double y){
-        // TODO: Implement the subtraction operation.
-        return -1;
+        return x - y;
     }
 
     // This method should multiply x by y and return the result.
     public double multiply(double x, double y){
-        // TODO: Implement the multiplication operation.
-        return -1;
+        return x * y;
     }
 
     // This method should divide x by y and return the result.
     // Remember to handle the case when y is zero to avoid division by zero.
     public double divide(double x, double y){
-        // TODO: Implement the division operation.
-        // TODO: Handle the divide by zero scenario.
-        return -1;
+        if (x == 0 || y == 0) {
+            throw new ArithmeticException("You cannot divide by zero.");
+        } else {
+            return x / y;
+        }
     }
 
     // This method should return the sine of x.
     public double sin(double x){
-        // TODO: Implement the sine function using Java's Math library or another method.
-        return -1;
+        return Math.sin(x);
     }
 
     // This method should return the cosine of x.
     public double cos(double x){
-        // TODO: Implement the cosine function using Java's Math library or another method.
-        return -1;
+        return Math.cos(x);
     }
 
     // This method should return the tangent of x.
     public double tan(double x){
-        // TODO: Implement the tangent function using Java's Math library or another method.
-        return -1;
+        return Math.tan(x);
     }
 
     // This method should raise x to the power of e and return the result.
     public double exponent(double x, double e){
-        // TODO: Implement the exponentiation operation.
-        return -1;
+        return Math.pow(x,e);
     }
 
     // This method should return the natural logarithm (base e) of x.
     // Remember to handle cases where x is negative or zero.
     public double logarithm(double x){
-        // TODO: Implement the natural logarithm function.
-        // TODO: Handle cases where x is less than or equal to zero.
-        return -1;
+        if (x <= 0){
+            throw new ArithmeticException("You cannot divide by zero.");
+        }
+        return Math.log(x);
     }
 }
